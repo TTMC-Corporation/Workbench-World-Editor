b43ce4acb728e827abb6116a95ad6bcda2186ad0
TheBlueLines <davidfeher42@gmail.com>
2023-04-27 20:45:49 +0200

    Add project files.

---
+++ Editor.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_CsLo;
+
+namespace Workbench_World_Editor
+{
+    public class Editor
+    {
+        public static void DoWork()
+        {
+            Raylib.InitWindow(800, 480, "Workbench World Editor");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Raylib.BLACK);
+
+                Raylib.DrawFPS(10, 10);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
